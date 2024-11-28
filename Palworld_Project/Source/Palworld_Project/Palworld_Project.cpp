ff9b83302475855d4c14be1d1579ee022c0b66b0
Naio <alsrb1002@naver.com>
2024-11-28 10:53:43 +0900

    Test

---
+++ Palworld_Project/Source/Palworld_Project/Palworld_Project.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Palworld_Project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Palworld_Project, "Palworld_Project" );
